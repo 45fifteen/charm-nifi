ec4eb27b37453ca00989a9cb26fd122d0a6574c3
45fifteen <45fifteen@gmail.com>
2015-11-23 10:48:19 -0500

    Create README.ex
    initial commit

---
+++ README.ex
@@ -0,0 +1,21 @@
+# Overview
+Apache nifi supports powerful and scalable directed graphs of data routing, transformation, and system mediation logic. Some of the high-level capabilities and objectives of Apache NiFi include:
+Web-based user interface
+	Seamless experience between design, control, feedback, and monitoring
+Highly configurable
+	Loss tolerant vs guaranteed delivery
+	Low latency vs high throughput
+	Dynamic prioritization
+	Flow can be modified at runtime
+	Back pressure
+Data Provenance
+	Track dataflow from beginning to end
+Designed for extension
+	Build your own processors and more
+	Enables rapid development and effective testing
+Secure
+	SSL, SSH, HTTPS, encrypted content, etc...
+	Pluggable role-based authentication/authorization
+# Installation
+ To deploy this charm you will need at a minimum: a cloud environment,
+ working Juju installation and a successful bootstrap.
